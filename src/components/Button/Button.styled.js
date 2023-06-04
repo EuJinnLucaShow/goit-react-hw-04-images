875504401536db89653aee74671499cb00e54dae
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-04 05:02:16 +0300

    Fixed the styles
    Fixed the styles

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Btn = styled.button`
   margin: auto;
   padding: 8px 16px;
-  border-radius: 2px;
+  border-radius: 3px;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   text-align: center;
